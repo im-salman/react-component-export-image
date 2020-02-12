a2bb1b6f6bcf495697ec9a139ec5daa475c788c5
Salman Siddiqui <ssiddiqui@monetate.com>
2020-02-12 11:52:58 -0500

    Init

--- README.md
+++ README.md
@@ -1,1 +1,31 @@
-# react-component-export-image
+# Usage
+
+```
+import { exportComponentAsJPEG, exportComponentAsPDF, exportComponentAsPNG } from "react-component-export-image";
+import React from "react";
+
+class MyComponent extends React.Component {
+  constructor(props) {
+    super(props);
+    this.componentRef = React.createRef();
+  }
+
+  render() {
+    return 
+    <React.Fragment>
+        <ComponentToPrint ref={this.componentRef} />
+        <button onClick={() => exportComponentAsJPEG(this.componentRef)}>
+            Export As JPEG
+        </button>
+        <button onClick={() => exportComponentAsPDF(this.componentRef)}>
+            Export As PDF
+        </button>
+        <button onClick={() => exportComponentAsPNG(this.componentRef)}>
+            Export As PNG
+        </button>
+    </React.Fragment>
+  }
+}
+
+```
+
---
+++ index.js
@@ -0,0 +1,60 @@
+import html2canvas from 'html2canvas';
+import JsPDF from 'jspdf';
+import ReactDOM from 'react-dom';
+
+const fileType = {
+    PNG: 'image/png',
+    JPEG: 'image/jpeg',
+    PDF: 'application/pdf'
+};
+
+const saveAs = (uri, filename) => {
+    const link = document.createElement('a');
+
+    if (typeof link.download === 'string') {
+        link.href = uri;
+        link.download = filename;
+        document.body.appendChild(link);
+        link.click();
+        document.body.removeChild(link);
+    } else {
+        window.open(uri);
+    }
+};
+
+const exportComponent = (node, fileName, backgroundColor, type) => {
+    const element = ReactDOM.findDOMNode(node.current);
+    return html2canvas(element, {
+        backgroundColor: backgroundColor,
+        scrollY: -window.scrollY,
+        useCORS: true,
+    }).then(canvas => {
+        if (type === fileType.PDF) {
+            const pdf = canvas.width > canvas.height
+                ? new JsPDF('l', 'mm', [canvas.width, canvas.height])
+                : new JsPDF('p', 'mm', [canvas.height, canvas.width]);
+            pdf.addImage(canvas.toDataURL(fileType.PNG, 1.0), 'PNG', 0, 0);
+            pdf.save(fileName);
+        } else {
+            saveAs(canvas.toDataURL(type, 1.0), fileName);
+        }
+    });
+};
+
+const exportComponentAsPNG = (node, fileName = 'component.png', backgroundColor = null, type = fileType.PNG) => {
+    exportComponent(node, fileName, backgroundColor, type);
+};
+
+const exportComponentAsJPEG = (node, fileName = 'component.jpeg', backgroundColor = null, type = fileType.JPEG) => {
+    exportComponent(node, fileName, backgroundColor, type);
+};
+
+const exportComponentAsPDF = (node, fileName = 'component.pdf', backgroundColor = null, type = fileType.PDF) => {
+    exportComponent(node, fileName, backgroundColor, type);
+};
+
+module.exports = { 
+    exportComponentAsJPEG, 
+    exportComponentAsPDF, 
+    exportComponentAsPNG 
+};
